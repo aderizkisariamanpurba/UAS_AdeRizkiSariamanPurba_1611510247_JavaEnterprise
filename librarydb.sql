d9ad7231ab08c2665aa94b4b3c2e57ba2c6d2292
Anang Kurniawan <161510163@student.budiluhur.ac.id>
2019-06-16 01:53:24 +0700

    UAS_AdeRizkiSariamanPurba_1611510247_JavaEnterprise

---
+++ librarydb.sql
@@ -0,0 +1,41 @@
+﻿# Host: localhost  (Version 5.5.5-10.1.16-MariaDB)
+# Date: 2019-06-15 22:43:48
+# Generator: MySQL-Front 5.4  (Build 3.46)
+# Internet: http://www.mysqlfront.de/
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "books"
+#
+
+DROP TABLE IF EXISTS `books`;
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `nm_book` varchar(30) NOT NULL,
+  `id_cat` int(11) NOT NULL,
+  `deskripsi` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Structure for table "categories"
+#
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `kode_kt` varchar(30) NOT NULL,
+  `deskripsi` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Structure for table "hibernate_sequence"
+#
+
+DROP TABLE IF EXISTS `hibernate_sequence`;
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
